5d3ab15be122e3f5f0bf7368bbce6be3308a6693
Anil Kanike <kanike.anil@gmail.com>
2016-02-22 20:29:53 +1100

    UAT - added equals, dashcode

--- src/main/java/com/learn/springmvcangularjs/beans/Train.java
+++ src/main/java/com/learn/springmvcangularjs/beans/Train.java
@@ -52,4 +52,27 @@ public class Train {
     public void setDiesel(Boolean diesel) {
         this.diesel = diesel;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Train)) return false;
+
+        Train train = (Train) o;
+
+        if (!id.equals(train.id)) return false;
+        if (!name.equals(train.name)) return false;
+        if (!speed.equals(train.speed)) return false;
+        return diesel.equals(train.diesel);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + speed.hashCode();
+        result = 31 * result + diesel.hashCode();
+        return result;
+    }
 }
